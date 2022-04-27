2a94fc43fc45286a0d85951467360928e4ce06e5
BuildTools <unconfigured@null.spigotmc.org>
2022-04-27 18:44:39 +0200

    first commit

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git add dist -f
+git commit -m 'deploy'
+
+git subtree push --prefix dist origin gh-pages
+
+cd -
